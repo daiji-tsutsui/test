1d503daedf4c0fe5a8cd8c7660a21d1652bd4d63
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2018-08-01 12:20:57 +0900

    Initial Commit

---
+++ git_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  git_test
+//
+//  Created by 筒井 大二 on 2018/08/01.
+//  Copyright © 2018年 筒井 大二. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
